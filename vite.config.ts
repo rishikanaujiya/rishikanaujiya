588f1354cc8346aee7ced048ceed5559ad13a765
Rishi Kanaujiya <145933402+rishikanaujiya@users.noreply.github.com>
2025-08-25 03:07:38 +0530

    Update vite.config.ts
    Signed-off-by: Rishi Kanaujiya <145933402+rishikanaujiya@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/", // ✅ custom domain
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"), // ⬅️ makes "@/..." work
     },
   },
+  // If deploying to custom domain use "/"
+  // If deploying to GitHub Pages use "/<repo>/"
+  base: "./", 
 });
