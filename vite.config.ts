f251e2be1836bcd80d69675cf98a33c751166091
Rishi Kanaujiya <145933402+rishikanaujiya@users.noreply.github.com>
2025-08-24 18:12:45 +0530

    Update vite.config.ts
    Signed-off-by: Rishi Kanaujiya <145933402+rishikanaujiya@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+// import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
+  base: "/", // <-- Add this line
   server: {
     host: "::",
-    port: 8080,
+    port: 5173,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+    // componentTagger(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
